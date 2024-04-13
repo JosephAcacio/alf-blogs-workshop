8f9c9d748de6dfebcc99b2bf88b1244939cfc55d
Joseph Acacio <josephcacacio@iskolarngbayan.pup.edu.ph>
2024-04-13 11:28:49 +0800

    finished set-up server

--- server/config/db.js
+++ server/config/db.js
@@ -1,1 +1,15 @@
 // db here
+const mongoose = require('mongoose');
+const dataBaseUrl = process.env.DATABASE_URL
+
+const connectDb = async() => {
+    try {
+        await mongoose.connect(dataBaseUrl);
+
+        console.log("Connection of Database: Success");
+    }catch (error){
+        console.log(error);
+    }
+}
+
+module.exports = connectDb;
