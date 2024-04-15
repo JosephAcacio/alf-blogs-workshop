920b854a3cecb1df5f364e6f3abfb48df1e2e9df
Joseph Acacio <josephcacacio@iskolarngbayan.pup.edu.ph>
2024-04-15 23:35:31 +0800

    finished footer and navbar set-up

---
+++ client/src/components/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+const App = () => {
+    return (
+        <div className="wrapper">
+            <Navbar />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
