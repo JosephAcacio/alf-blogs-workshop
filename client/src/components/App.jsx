0a32b12a3b65972d83710c970d6e71c634af76d2
Joseph Acacio <josephcacacio@iskolarngbayan.pup.edu.ph>
2024-04-17 14:33:56 +0800

    finished Cta Section

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,6 +2,7 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Hero from "./Hero";
 import ArticleSection from "./ArticleSection";
+import CtaSection from "./CtaSection";
 
 const App = () => {
     return (
@@ -9,6 +10,7 @@ const App = () => {
             <Navbar />
             <Hero />
             <ArticleSection />
+            <CtaSection />
             <Footer />
         </div>
     );
